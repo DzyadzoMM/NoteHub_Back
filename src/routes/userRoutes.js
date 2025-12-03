9f3c02351c25bf4a6d38a2bd356dbea18d1e9f45
Myroslav Dzyadzo <122197686+DzyadzoMM@users.noreply.github.com>
2025-12-03 20:55:20 +0200

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import {updateUserAvatar} from '../controllers/userController.js';
+import {updateUserAvatar, getCurrentUser} from '../controllers/userController.js';
 import { upload } from '../middleware/multer.js';
 import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
+
+router.get('/users/me', authenticate, getCurrentUser);
 
 router.patch('/users/me/avatar', authenticate, upload.single("avatar") ,updateUserAvatar);
 
